cc15d5e21cc9c8ec444004aaab456ad3760a8a78
takaoka <pinokions009@gmail.com>
2021-03-30 06:20:05 +0900

    shellではエラー時に落ちないようにした

--- shell.hs
+++ shell.hs
@@ -10,11 +10,12 @@ shell binds = do
     str <- getLine
     case _eval binds (toExp str) of
         Left s -> do putStrLn s
+        Right ((Err e: []), _) -> do
+            putStrLn $ "Error: " ++ e
+            shell binds
         Right ((res: []), binds2) -> do
             print res
             shell binds2
-        Right ((Err e: []), _) -> do
-            putStrLn $ "Error: " ++ e
         Right ([], binds2) -> do
             shell binds2
         Right (expr, _) -> do
