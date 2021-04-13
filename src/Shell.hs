3dc402ab7653d3a613165a2fc1a932d87b514252
YouTakaoka <pinokions009@gmail.com>
2021-04-13 21:50:33 +0900

    Shellでも複数行入力ができるようにした。

--- src/Shell.hs
+++ src/Shell.hs
@@ -6,11 +6,23 @@ import System.IO
 prompt :: IO ()
 prompt = putStr "Topos> " >> hFlush stdout
 
-shell :: [Bind] -> IO ()
-shell binds = do
+getExpression :: IO Exp
+getExpression = do
     prompt
     str <- getLine
-    case _eval M_Normal binds (toExp str) of
+    let expr = toExp str
+    if null expr
+        then return expr
+        else
+            case last expr of
+                Tobe "\\" ->
+                    (init expr ++) <$> getExpression
+                _ -> return expr    
+
+shell :: [Bind] -> IO ()
+shell binds = do
+    expr <- getExpression
+    case _eval M_Normal binds expr of
         Error e -> do
             putStrLn $ "Error: " ++ show e
             shell binds
